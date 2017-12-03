b0c3311bc65b7273e8e9a0bbbfb3cb3bf13a7c6b
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-03 23:47:26 +0000

    Day3_Part1

---
+++ Solutions/Day3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public class Day3
+    {
+        public int CalculateDistance(int square)
+        {
+            // Work out which layer on the square holds our value
+            var max = 1;
+            var height = 1;
+            while (square > max)
+            {
+                height += 2;
+                max = height * height;
+            }
+
+            // Work out which side of the square
+            var midOffset = (height - 1) / 2;
+            if (square > max - height)
+            {
+                // Bottom
+                return midOffset + Math.Abs((max - midOffset) - square);
+            }
+            else if (square > max - (height * 2) + 1)
+            {
+                //Left
+                return midOffset + Math.Abs((max - (height - 1) - midOffset) - square);
+            }
+            else if (square > max - (height * 3) + 2)
+            {
+                //Top
+                return midOffset + Math.Abs((max - ((height - 1) * 2) - midOffset) - square);
+            }
+            else
+            {
+                //Right
+                return midOffset + Math.Abs((max - ((height - 1) * 3) - midOffset) - square);
+            }
+        }
+    }
+}
---
+++ Tests/TestDay3.cs
@@ -0,0 +1,29 @@
+﻿using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay3
+    {
+        [Theory]
+        [InlineData(1, 0)]
+        [InlineData(12, 3)]
+        [InlineData(23, 2)]
+        [InlineData(1024, 31)]
+        public void ChecksAnswersToPartA(int square, int expectedResult)
+        {
+            var day3 = new Day3();
+            var actual = day3.CalculateDistance(square);
+            Assert.Equal(expectedResult, actual);
+        }
+
+        [Fact]
+        public void SolvePartA()
+        {
+            var day3 = new Day3();
+            var actual = day3.CalculateDistance(277678);
+            Assert.True(true);
+        }
+
+    }
+}
