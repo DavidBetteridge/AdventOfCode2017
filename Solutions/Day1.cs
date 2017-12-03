5e79cbe24109d42d4a00881f1a945ec3f11c1be1
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-03 22:42:05 +0000

    Day1

---
+++ Solutions/Day1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day1
+    {
+        public int ComputeSum(string input)
+        {
+            // Put the first number on the end to allow for the circular array
+            var fullInput = input + input.Substring(0, 1);
+            return input.ToCharArray()
+                        .Where((d, i) => d == fullInput[i + 1])
+                        .Sum(d => d - '0');
+        }
+
+        public int ComputeSumForPart2(string input)
+        {
+            var offset = input.Length / 2;
+            return input.ToCharArray()
+                        .Where((d, i) => d == input[(i + offset) % input.Length])
+                        .Sum(d => d - '0');
+        }
+    }
+}
