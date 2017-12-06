2f00d4192b501098fad7b980feb7a4be5c28f4ec
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-06 23:32:47 +0000

    Day6 Part 1

---
+++ Solutions/Day6.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day6
+    {
+        public string NextIteration(string given)
+        {
+            var numbers = given
+                            .Split(' ')
+                            .Select(a => int.Parse(a))
+                            .ToArray();
+
+            // Which block contains the highest allocation,  picking
+            // the first one in the case of a tie.
+            var largest = 0;
+            var largestBlock = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > largest)
+                {
+                    largest = numbers[i];
+                    largestBlock = i;
+                }
+            }
+
+            // Empty out the highest block
+            numbers[largestBlock] = 0;
+
+            // Allocate the amount to each block in turn.
+            //var index = (largestBlock + 1) % numbers.Count();
+            //var leftToAllocate = largest;
+            //while (leftToAllocate > 0)
+            //{
+            //    numbers[index]++;
+            //    leftToAllocate--;
+            //    index = (index + 1) % numbers.Count();
+            //}
+
+            var sharedAllocation = largest / numbers.Count();
+            var leftOver = largest % numbers.Count();
+            for (int i = largestBlock + 1; i < numbers.Length + largestBlock + 1; i++)
+            {
+                numbers[i % numbers.Length] += sharedAllocation;
+                if (leftOver > 0)
+                {
+                    numbers[i % numbers.Length]++;
+                    leftOver--;
+                }
+            }
+
+            return string.Join(" ", numbers);
+        }
+
+        public int RepeatsAfter(string given)
+        {
+            var seen = new HashSet<string>();
+            var currentPattern = given;
+            var result = 0;
+
+            while (!seen.Contains(currentPattern))
+            {
+                seen.Add(currentPattern);
+                result++;
+                currentPattern = NextIteration(currentPattern);
+            }
+
+            return result;
+
+        }
+    }
+}
---
+++ Tests/TestDay6.cs
@@ -0,0 +1,43 @@
+﻿using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay6
+    {
+        [Theory]
+        [InlineData("0 2 7 0", "2 4 1 2")]
+        [InlineData("2 4 1 2", "3 1 2 3")]
+        [InlineData("3 1 2 3", "0 2 3 4")]
+        [InlineData("0 2 3 4", "1 3 4 1")]
+        [InlineData("1 3 4 1", "2 4 1 2")]
+        public void Part1_NextIteration(string given, string expect)
+        {
+            var day6 = new Day6();
+            var actual = day6.NextIteration(given);
+
+            Assert.Equal(expect, actual);
+        }
+
+        [Theory]
+        [InlineData("0 2 7 0", 5)]
+        public void Part1(string given, int expect)
+        {
+            var day6 = new Day6();
+            var actual = day6.RepeatsAfter(given);
+
+            Assert.Equal(expect, actual);
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day6 = new Day6();
+            var actual = day6.RepeatsAfter(TEST_DATA);
+
+            Assert.Equal(100, actual);
+        }
+
+        private const string TEST_DATA = @"14 0 15 12 11 11 3 5 1 6 8 4 9 1 8 4";
+    }
+}
