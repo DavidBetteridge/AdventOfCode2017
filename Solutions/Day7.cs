e289c25bb5a5c389daead1d24f137896b66e862d
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-07 22:40:59 +0000

    Day 7 Part 2

--- Solutions/Day7.cs
+++ Solutions/Day7.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Solutions
@@ -7,12 +8,21 @@ namespace Solutions
     {
         public string FindBase(string given)
         {
+            var rootNode = BuildTree(given);
+            return rootNode.Name;
+        }
+
+        private static Node BuildTree(string given)
+        {
             var nodes = new Dictionary<string, Node>();
             var lines = given.Split(new[] { System.Environment.NewLine }, System.StringSplitOptions.RemoveEmptyEntries);
             foreach (var line in lines)
             {
                 var name = line.Substring(0, line.IndexOf(' '));
-                var newNode = new Node() { Name = name , Children=new List<Node>()};
+
+                var s = line.IndexOf('(') + 1;
+                var weight = line.Substring(s, line.IndexOf(')') - s);
+                var newNode = new Node() { Name = name, Children = new List<Node>(),  Weight= int.Parse(weight) };
                 nodes.Add(name, newNode);
             }
 
@@ -36,10 +46,42 @@ namespace Solutions
                 node = node.Parent;
             }
 
-
-            return node.Name;
+            return node;
         }
 
+        public int FindIdealWeight(string given)
+        {
+            var rootNode = BuildTree(given);
+
+            return CalculateWeights(rootNode);
+        }
+ 
+
+        private int CalculateWeights(Node node)
+        {
+            node.TotalWeight = node.Weight;
+            if (!node.Children.Any()) return -1;  //Leaf node
+
+            foreach (var child in node.Children)
+            {
+                var adjustment = CalculateWeights(child);
+                if (adjustment != -1) return adjustment;
+                node.TotalWeight += child.TotalWeight;
+            }
+
+            // Do we have a problem with any of our children?
+            var children = node.Children.GroupBy(a => a.TotalWeight);
+            if (children.Count() > 1)
+            {
+                // One of our children has a different value
+                var wrongChild = children.First(a => a.Count() == 1).First();
+                var correctChild = children.First(a => a.Count() > 1).First();
+                var adjustment = correctChild.TotalWeight - wrongChild.TotalWeight;
+                return wrongChild.Weight + adjustment;
+            }
+
+            return -1;
+        }
 
         private class Node
         {
@@ -48,6 +90,8 @@ namespace Solutions
 
             public Node Parent { get; set; }
             public List<Node> Children { get; set; }
+
+            public int TotalWeight { get; set; }
         }
     }
 }
--- Tests/TestDay7.cs
+++ Tests/TestDay7.cs
@@ -40,6 +40,40 @@ cntj (57)";
             Assert.Equal("vgzejbd", actual);
         }
 
+
+        [Fact]
+        public void Part2()
+        {
+            var given = @"pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)";
+
+            var day7 = new Day7();
+            var actual = day7.FindIdealWeight(given);
+
+            Assert.Equal(60, actual);
+        }
+
+        [Fact]
+        public void Part2_Answer()
+        {
+            var day7 = new Day7();
+            var actual = day7.FindIdealWeight(TEST_INPUT);
+
+            Assert.Equal(0, actual);
+        }
+
+
         private const string TEST_INPUT = @"wdysq (135) -> sxldvex, wiasj
 vjwuuft (33) -> inuci, neddz, rwamq
 oislgqy (77)
