2ad2aecdc49b148ef73b0f107d1e6ecd19953cac
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-10 16:17:38 +0000

    Day 10 - Part 1

---
+++ Solutions/Day10.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Solutions
+{
+    public class Day10
+    {
+        public object Solve(int numberOfElements, string lengths)
+        {
+            // Build a list with the elements 0... numberOfElemet-1
+            var list = new List<int>(numberOfElements);
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                list.Add(i);
+            }
+
+
+            // Process each length in turn
+            var currentPosition = 0;
+            var skipSize = 0;
+            foreach (var l in lengths.Split(','))
+            {
+                var length = int.Parse(l);
+
+                var temp = new List<int>();
+                for (int elementNumber = 0; elementNumber < length; elementNumber++)
+                {
+                    temp.Add(list[(currentPosition + elementNumber) % numberOfElements]);
+                }
+                for (int elementNumber = 0; elementNumber < length; elementNumber++)
+                {
+                    var rhs = ((currentPosition + length) - elementNumber - 1) % numberOfElements;
+                    list[rhs] = temp[elementNumber];
+                }
+
+                currentPosition += (length + skipSize);
+                currentPosition = currentPosition % numberOfElements;
+                skipSize++;
+            }
+
+            return list[0] * list[1];
+        }
+    }
+}
---
+++ Tests/TestDay10.cs
@@ -0,0 +1,29 @@
+﻿using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay10
+    {
+
+        [Fact]
+        public void Part1()
+        {
+            var day10 = new Day10();
+            var actual = day10.Solve(5, "3,4,1,5");
+            Assert.Equal(12, actual);
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day10 = new Day10();
+            var actual = day10.Solve(256, PUZZLE_INPUT);
+            Assert.Equal(12, actual);
+        }
+
+
+        private const string PUZZLE_INPUT = "147,37,249,1,31,2,226,0,161,71,254,243,183,255,30,70";
+
+    }
+}
