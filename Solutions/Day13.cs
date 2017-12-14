42b0e3f8cbdc7e1d1ed3fbfb7acfbd871cf4b0cf
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-14 01:00:32 +0000

    Day13 Part 2

--- Solutions/Day13.cs
+++ Solutions/Day13.cs
@@ -1,74 +1,56 @@
-﻿namespace Solutions
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
 {
     public class Day13
     {
         public int Calculate(string input)
         {
+            var all = Build(input);
+
+            return all
+                    .Where(location => ((location.Position) % (2 * (location.TotalDepth - 1))) == 0)
+                    .Sum(l => l.Position * l.TotalDepth);
+        }
+
+        private static List<Location> Build(string input)
+        {
             var depths = input.Split(new[] { System.Environment.NewLine }, System.StringSplitOptions.RemoveEmptyEntries);
 
-            var all = new Location[100];
+            var all = new List<Location>();
             foreach (var dep in depths)
             {
                 var parts = dep.Split(':');
                 var location = int.Parse(parts[0]);
                 var depth = int.Parse(parts[1].Trim());
-                all[location] = new Location() { CurrentLocation = 0, Increasing = true, TotalDepth = depth };
+                all.Add(new Location() { Position = location, TotalDepth = depth });
             }
 
-            var hits = 0;
-            for (int picosecond = 0; picosecond < 100; picosecond++)
+            return all;
+        }
+
+        public int Escape(string input, int finalLocation)
+        {
+            var all = Build(input);
+
+            var delay = -1;
+            while (true)
             {
-                var here = all[picosecond];
-                if (here != null && here.CurrentLocation == 0)
+                delay++;
+                if (!all.Exists((location => ((location.Position+delay) % (2 * (location.TotalDepth - 1))) == 0)))
                 {
-                    hits += picosecond * here.TotalDepth;
+                    return delay;
                 }
-
-                for (int loc = 0; loc < 100; loc++)
-                {
-                    var location = all[loc];
-                    if (location != null)
-                    {
-                        if (location.Increasing)
-                        {
-                            var newLocation = location.CurrentLocation + 1;
-                            if (newLocation >= location.TotalDepth)
-                            {
-                                location.CurrentLocation--;
-                                location.Increasing = false;
-                            }
-                            else
-                            {
-                                location.CurrentLocation++;
-                            }
-                        }
-                        else
-                        {
-                            var newLocation = location.CurrentLocation - 1;
-                            if (newLocation < 0)
-                            {
-                                location.CurrentLocation++;
-                                location.Increasing = true;
-                            }
-                            else
-                            {
-                                location.CurrentLocation--;
-                            }
-                        }
-                    }
-                }
-
-
             }
 
-            return hits;
         }
 
         class Location
         {
             public int TotalDepth { get; set; }
-            public int CurrentLocation { get; set; }
-            public bool Increasing { get; set; }
+            public int Position { get; set; }
         }
     }
 }
--- Tests/TestDay13.cs
+++ Tests/TestDay13.cs
@@ -17,11 +17,30 @@ namespace Tests
         }
 
         [Fact]
+        public void Part2()
+        {
+            var day13 = new Day13();
+            var actual = day13.Escape(@"0: 3
+1: 2
+4: 4
+6: 4",6);
+            Assert.Equal(10, actual);
+        }
+
+        [Fact]
         public void Part1_Answer()
         {
             var day13 = new Day13();
             var actual = day13.Calculate(PUZZLE_INPUT);
-            Assert.Equal(24, actual);
+            Assert.Equal(1632, actual);
+        }
+
+        [Fact]
+        public void Part2_Answer()
+        {
+            var day13 = new Day13();
+            var actual = day13.Escape(PUZZLE_INPUT,98);
+            Assert.Equal(3834136, actual);
         }
 
         private const string PUZZLE_INPUT = @"0: 4
