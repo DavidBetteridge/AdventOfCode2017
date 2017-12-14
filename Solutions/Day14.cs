dcb5f5cfac244e6d8c589a2c67541ac99bcd2391
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-14 08:23:10 +0000

    Day14 Part 1

---
+++ Solutions/Day14.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solutions
+{
+    public class Day14
+    {
+        public string CalculateHash(string input)
+        {
+            // Convert the input to ascii codes
+            var finalInput = new byte[input.Length + 5];
+            for (int i = 0; i < input.Length; i++)
+            {
+                finalInput[i] = ((byte)input[i]);
+            }
+            Array.Copy(new byte[5] { 17, 31, 73, 47, 23 }, 0, finalInput, input.Length, 5);
+
+            // Build a list with the elements 0... numberOfElements-1
+            const int numberOfElements = 256;
+            var list = BuildListOfNumbers(numberOfElements);
+
+            // Apply the rounds
+            var currentPosition = 0;
+            var skipSize = 0;
+            for (int round = 0; round < 64; round++)
+            {
+                (currentPosition, skipSize) = DoRound(finalInput, list, currentPosition, skipSize);
+            }
+
+            // Create the dense hash
+            return CalculateDenseHash(numberOfElements, list);
+        }
+
+        public int CountUsedSquares(string key)
+        {
+            var result = 0;
+            for (int row = 0; row < 128; row++)
+            {
+                var hash = CalculateHash($"{key}-{row}");
+                var binary = HashToBinary(hash);
+                result+= binary.Replace("0", "").Length;
+            }
+            return result;
+        }
+
+        public string HashToBinary(string hexstring)
+        {
+            return String.Join(String.Empty,
+  hexstring.Select(
+    c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
+  )
+);
+        }
+
+        private static List<int> BuildListOfNumbers(int numberOfElements)
+        {
+            // Build a list with the elements 0... numberOfElements-1
+            var list = new List<int>(numberOfElements);
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                list.Add(i);
+            }
+
+            return list;
+        }
+
+        private static string CalculateDenseHash(int numberOfElements, List<int> list)
+        {
+            var denseHash = "";
+            for (int blockStart = 0; blockStart < numberOfElements; blockStart += 16)
+            {
+                var runningTotal = list[blockStart];
+                for (int e = 1; e < 16; e++)
+                {
+                    runningTotal ^= list[blockStart + e];
+                }
+                denseHash += runningTotal.ToString("X2");
+            }
+
+            return denseHash.ToLower(); ;
+        }
+
+        private static (int currentPosition, int skipSize) DoRound(byte[] lengths, List<int> list, int currentPosition, int skipSize)
+        {
+            var numberOfElements = list.Count;
+            foreach (var length in lengths)
+            {
+                var temp = new List<int>();
+                for (int elementNumber = 0; elementNumber < length; elementNumber++)
+                {
+                    temp.Add(list[(currentPosition + elementNumber) % numberOfElements]);
+                }
+                for (int elementNumber = 0; elementNumber < length; elementNumber++)
+                {
+                    var rhs = ((currentPosition + length) - elementNumber - 1) % numberOfElements;
+                    list[rhs] = temp[elementNumber];
+                }
+
+                currentPosition += (length + skipSize);
+                currentPosition = currentPosition % numberOfElements;
+                skipSize++;
+            }
+
+            return (currentPosition, skipSize);
+        }
+    }
+}
---
+++ Tests/TestDay14.cs
@@ -0,0 +1,44 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay14
+    {
+        [Fact]
+        public void CalculateHash()
+        {
+            var day14 = new Day14();
+            var hash = day14.CalculateHash("flqrgnkx-0");
+        }
+
+        [Fact]
+        public void HashToBinary()
+        {
+            var day14 = new Day14();
+            var hash = day14.HashToBinary("a0c2017");
+            Assert.True(hash.StartsWith("1010000011000010000000010111"));
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var day14 = new Day14();
+            var actual = day14.CountUsedSquares("flqrgnkx");
+            Assert.Equal(8108, actual);
+
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day14 = new Day14();
+            var actual = day14.CountUsedSquares("jzgqcdpd");
+            Assert.Equal(8074, actual);
+
+        }
+    }
+}
