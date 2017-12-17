85ba5f8c64884cbf84e50a3c00b4fc6141028bf3
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-17 15:42:44 +0000

    Day17 - Part 1

---
+++ Solutions/Day17.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solutions
+{
+    public class Day17
+    {
+        public (List<int> newState, int newPosition) Spin(List<int> initialState, int initialPosition, int numberOfSteps, int nextValue)
+        {
+            var location = (initialPosition + numberOfSteps) % initialState.Count;
+            initialState.Insert(location + 1, nextValue);
+            return (initialState, location + 1);
+        }
+
+        public int Part1(int numberOfSteps)
+        {
+            var state = new List<int>() { 0 };
+            var position = 0;
+
+            for (int nextValue = 1; nextValue <= 2017; nextValue++)
+            {
+                (state, position) = Spin(state, position, numberOfSteps, nextValue);
+            }
+
+            return state[(position + 1) % state.Count];
+        }
+    }
+}
---
+++ Tests/TestDay17.cs
@@ -0,0 +1,52 @@
+﻿using Solutions;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay17
+    {
+        [Fact]
+        public void Spin1()
+        {
+            var day17 = new Day17();
+            var (newState, newPosition) = day17.Spin(new List<int>() { 0 }, 0, 3, 1);
+            Assert.Equal(1, newPosition);
+            Assert.Equal(new List<int>() { 0, 1 }, newState);
+        }
+
+        [Fact]
+        public void Spin2()
+        {
+            var day17 = new Day17();
+            var (newState, newPosition) = day17.Spin(new List<int>() { 0, 1 }, 1, 3, 2);
+            Assert.Equal(1, newPosition);
+            Assert.Equal(new List<int>() { 0, 2, 1 }, newState);
+        }
+
+        [Fact]
+        public void Spin3()
+        {
+            var day17 = new Day17();
+            var (newState, newPosition) = day17.Spin(new List<int>() { 0, 2, 1 }, 1, 3, 3);
+            Assert.Equal(2, newPosition);
+            Assert.Equal(new List<int>() { 0, 2, 3, 1 }, newState);
+        }
+
+        [Fact]
+        public void Part1()
+        {
+            var day17 = new Day17();
+            var actual = day17.Part1(3);
+            Assert.Equal(638, actual);
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day17 = new Day17();
+            var actual = day17.Part1(329);
+            Assert.Equal(725, actual);
+        }
+    }
+}
