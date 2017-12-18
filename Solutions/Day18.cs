cc603377836711723ab5764be7b72bdda465dddd
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-18 08:43:19 +0000

    Day18 - Part 2

--- Solutions/Day18.cs
+++ Solutions/Day18.cs
@@ -6,6 +6,139 @@ namespace Solutions
 {
     public class Day18
     {
+
+        public long Solve2(string input)
+        {
+            var commands = input.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            var registers0 = new Dictionary<char, long>();
+            var registers1 = new Dictionary<char, long>();
+
+            var programCounter0 = 0L;
+            var programCounter1 = 0L;
+
+            SetValue('p', registers0, 0);
+            SetValue('p', registers1, 1);
+
+            var queueFrom0 = new Queue<long>();
+            var queueFrom1 = new Queue<long>();
+
+            var workDone0 = true;
+            var workDone1 = true;
+
+            var numberSent1To0 = 0;
+
+            while (workDone0 || workDone1)
+            {
+                var temp = 0;
+                (programCounter0, workDone0, _) = RunUntilHalted(registers0, programCounter0, commands, queueFrom0, queueFrom1);
+                (programCounter1, workDone1, temp) = RunUntilHalted(registers1, programCounter1, commands, queueFrom1, queueFrom0);
+                numberSent1To0 += temp;
+            }
+
+            return numberSent1To0;
+
+        }
+
+        private (long pc, bool workdone, int numberSent) RunUntilHalted(Dictionary<char, long> registers,
+                                    long programCounter,
+                                    string[] commands,
+                                    Queue<long> readFrom,
+                                    Queue<long> sendTo)
+        {
+            var workDone = false;
+            var numberSent = 0;
+            while (true)
+            {
+                if (programCounter >= commands.Length)
+                {
+                    // No more commands
+                    return (programCounter, workDone, numberSent);
+                }
+
+                var command = commands[programCounter];
+
+                var parts = command.Split(' ');
+                var lhs = 0L;
+                var rhs = 0L;
+                switch (parts[0])
+                {
+                    case "add":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs + rhs);
+                        programCounter++;
+                        workDone = true;
+                        break;
+
+                    case "set":
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, rhs);
+                        programCounter++;
+                        workDone = true;
+
+                        break;
+
+                    case "mul":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs * rhs);
+                        programCounter++;
+                        workDone = true;
+                        break;
+
+                    case "mod":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs % rhs);
+                        programCounter++;
+                        workDone = true;
+                        break;
+
+                    case "snd":
+                        sendTo.Enqueue(ReadValue(registers, parts[1]));
+                        programCounter++;
+                        workDone = true;
+                        numberSent++;
+                        break;
+
+                    case "rcv":
+                        if (readFrom.Count > 0)
+                        {
+                            SetValue(parts[1][0], registers, readFrom.Dequeue());
+                            programCounter++;
+                            workDone = true;
+                        }
+                        else
+                        {
+                            // Nothing on the queue - we need to context switch
+                            return (programCounter, workDone, numberSent);
+                        }
+                        break;
+
+                    case "jgz":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        if (lhs > 0)
+                        {
+                            programCounter += rhs;
+                        }
+                        else
+                        {
+                            programCounter++;
+                        }
+                        workDone = true;
+                        break;
+
+                    default:
+                        throw new Exception("Unknown command " + command);
+                }
+            }
+
+
+        }
+
+
         public long Solve(string input)
         {
             var commands = input.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
@@ -79,6 +212,7 @@ namespace Solutions
 
 
         }
+
 
         private void SetValue(char register, Dictionary<char, long> registers, long newValue)
         {
--- Tests/TestDay18.cs
+++ Tests/TestDay18.cs
@@ -28,7 +28,29 @@ jgz a -2";
         {
             var day18 = new Day18();
             var actual = day18.Solve(PUZZLE_INPUT);
-            Assert.Equal(4, actual);
+            Assert.Equal(3423, actual);
+        }
+
+        [Fact]
+        public void Part2()
+        {
+            var day18 = new Day18();
+            var actual = day18.Solve2(@"snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d");
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void Part2_Answer()
+        {
+            var day18 = new Day18();
+            var actual = day18.Solve2(PUZZLE_INPUT);
+            Assert.Equal(7493, actual);
         }
 
         private const string PUZZLE_INPUT = @"set i 31
