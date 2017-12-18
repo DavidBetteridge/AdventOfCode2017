27bfe57d66da0ce0a4b2283c9a874254adee2d2e
DavidBetteridge <davidabetteridge@gmail.com>
2017-12-18 08:05:33 +0000

    Day 18 - Part 1

---
+++ Solutions/Day18.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public class Day18
+    {
+        public long Solve(string input)
+        {
+            var commands = input.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var registers = new Dictionary<char, long>();
+            var lastSound = 0L;
+            var programCounter = 0L;
+            while (true)
+            {
+                var command = commands[programCounter];
+
+                var parts = command.Split(' ');
+                var lhs = 0L;
+                var rhs = 0L;
+                switch (parts[0])
+                {
+                    case "add":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs + rhs);
+                        programCounter++;
+                        break;
+
+                    case "set":
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, rhs);
+                        programCounter++;
+                        break;
+
+                    case "mul":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs * rhs);
+                        programCounter++;
+                        break;
+
+                    case "mod":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        SetValue(parts[1][0], registers, lhs % rhs);
+                        programCounter++;
+                        break;
+
+                    case "snd":
+                        lastSound = ReadValue(registers, parts[1]);
+                        programCounter++;
+                        break;
+
+                    case "rcv":
+                        lhs = ReadValue(registers, parts[1]);
+                        if (lhs > 0) return lastSound;
+                        programCounter++;
+                        break;
+
+                    case "jgz":
+                        lhs = ReadValue(registers, parts[1]);
+                        rhs = ReadValue(registers, parts[2]);
+                        if (lhs > 0)
+                        {
+                            programCounter += rhs;
+                        }
+                        else
+                        {
+                            programCounter++;
+                        }
+                        break;
+
+                    default:
+                        throw new Exception("Unknown command " + command);
+                }
+            }
+
+
+        }
+
+        private void SetValue(char register, Dictionary<char, long> registers, long newValue)
+        {
+            registers[register] = newValue;
+        }
+
+        private long ReadValue(Dictionary<char, long> registers, string parm)
+        {
+            if (int.TryParse(parm, out var t))
+            {
+                return t;
+            }
+
+            var i = 0L;
+            registers.TryGetValue(parm[0], out i);
+            return i;
+        }
+    }
+}
---
+++ Tests/TestDay18.cs
@@ -0,0 +1,76 @@
+﻿using Solutions;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay18
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day18 = new Day18();
+            var commands = @"set a 1
+add a 2
+mul a a
+mod a 5
+snd a
+set a 0
+rcv a
+jgz a -1
+set a 1
+jgz a -2";
+            var actual = day18.Solve(commands);
+            Assert.Equal(4, actual); 
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day18 = new Day18();
+            var actual = day18.Solve(PUZZLE_INPUT);
+            Assert.Equal(4, actual);
+        }
+
+        private const string PUZZLE_INPUT = @"set i 31
+set a 1
+mul p 17
+jgz p p
+mul a 2
+add i -1
+jgz i -2
+add a -1
+set i 127
+set p 618
+mul p 8505
+mod p a
+mul p 129749
+add p 12345
+mod p a
+set b p
+mod b 10000
+snd b
+add i -1
+jgz i -9
+jgz a 3
+rcv b
+jgz b -1
+set f 0
+set i 126
+rcv a
+rcv b
+set p a
+mul p -1
+add p b
+jgz p 4
+snd a
+set a b
+jgz 1 3
+snd b
+set f 1
+add i -1
+jgz i -11
+snd a
+jgz f -16
+jgz a -19";
+    }
+}
