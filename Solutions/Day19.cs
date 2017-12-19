97ed972cb2a3306c3138ac725bd4b71c33969f1c
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-19 07:18:22 +0000

    Day 19 - Part 1

---
+++ Solutions/Day19.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solutions
+{
+    public class Day19
+    {
+        public string Walk(string diagram)
+        {
+            var lines = diagram.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Find start
+            var currentY = 0;
+            var currentX = lines[0].IndexOf('|');
+
+            var result = "";
+            var direction = Direction.South;
+
+            while (true)
+            {
+                switch (direction)
+                {
+                    case Direction.North:
+                        currentY--;
+                        break;
+                    case Direction.South:
+                        currentY++;
+                        break;
+                    case Direction.West:
+                        currentX--;
+                        break;
+                    case Direction.East:
+                        currentX++;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (currentX < 0 || currentX == lines[0].Length) break;
+                if (currentY < 0 || currentY == lines.Length) break;
+
+
+                var newChar = lines[currentY][currentX];
+                switch (newChar)
+                {
+                    case '|':
+                    case '-':
+                        //Keep going
+                        break;
+                    case '+':
+                        //Change direction
+                        if (direction == Direction.West || direction == Direction.East)
+                        {
+                            //Switch from w/e to n/s
+                            if (currentY > 0 && lines[currentY-1][currentX] != ' ')
+                                direction = Direction.North;
+                            else
+                                direction = Direction.South;
+                        }
+                        else
+                        {
+                            //Switch from n/s to e/w
+                            if (currentX > 0 && lines[currentY][currentX - 1] != ' ')
+                                direction = Direction.West;
+                            else
+                                direction = Direction.East;
+                        }
+                        break;
+                    case ' ':
+                        break;
+                    default:
+                        result += newChar;
+                        break;
+                }
+
+                if (newChar == ' ') break;
+
+            }
+
+
+            return result;
+        }
+
+        enum Direction
+        {
+            North = 0,
+            South = 1,
+            West = 2,
+            East = 3
+        }
+
+    }
+}
