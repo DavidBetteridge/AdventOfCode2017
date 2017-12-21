e6d7db50f77837b3a575feeac92d469c744b4fb2
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-21 16:47:00 +0000

    Day21 - Part 1

--- Solutions/Day21.cs
+++ Solutions/Day21.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 
@@ -51,35 +52,59 @@ namespace Solutions
             var r1 = RotateClockwise(patternArray);
             var r2 = RotateClockwise(r1);
             var r3 = RotateClockwise(r2);
-            var h1 = HFlip(patternArray);
-            var h2 = HFlip(r1);
-            var h3 = HFlip(r2);
-            var h4 = HFlip(r3);
-
-            return new List<string>() { pattern, ArrayToString(r1), ArrayToString(r2), ArrayToString(r3), ArrayToString(h1), ArrayToString(h2), ArrayToString(h3), ArrayToString(h4) };
+            var h1 = VFlip(patternArray);
+            var h2 = RotateClockwise(h1);
+            var h3 = RotateClockwise(h2);
+            var h4 = RotateClockwise(h3);
+            var v1 = VFlip(patternArray);
+            var v2 = RotateClockwise(v1);
+            var v3 = RotateClockwise(v2);
+            var v4 = RotateClockwise(v3);
+            return new List<string>() { pattern, ArrayToString(r1), ArrayToString(r2), ArrayToString(r3), ArrayToString(h1), ArrayToString(h2), ArrayToString(h3), ArrayToString(h4), ArrayToString(v1), ArrayToString(v2), ArrayToString(v3), ArrayToString(v4) };
 
         }
 
-        public int Part1(string input, string patterns)
+        public long Part1(string input, string patterns, int iterations)
         {
             var allPatterns = ExpandPatterns(patterns);
 
-            for (int iteration = 0; iteration < 5; iteration++)
+            var asArray = StringToArray(input);
+            for (int iteration = 0; iteration < iterations; iteration++)
             {
-                var (inputs, size) = SplitUpInput(input);
-                var updated = inputs.Select(a => allPatterns[a]);
-                input = RejoinInput(updated, size);
+                var inputs = SplitUpInput(asArray);
+                var updated = inputs.Select(a =>
+                {
+                    if (allPatterns.TryGetValue(a, out var p))
+                        return p;
+                    else
+                        throw new Exception("Unknown pattern " + a);
+                });
+                asArray = RejoinInput(updated);
             }
 
+            var result = 0L;
+            for (int column = 0; column <= asArray.GetUpperBound(0); column++)
+            {
+                for (int row = 0; row <= asArray.GetUpperBound(0); row++)
+                {
+                    if (asArray[column, row]) result++;
+                }
+            }
 
-            return input.Replace("/", "").Replace(".", "").Count();
+            return result;
 
         }
 
-        private string RejoinInput(IEnumerable<string> updated, int size)
+        private void display(string input)
+        {
+            Debug.WriteLine(input.Replace("/", System.Environment.NewLine));
+        }
+
+        private bool[,] RejoinInput(IEnumerable<string> updated)
         {
             var numberOfBlocks = updated.Count();
             var numberPerSide = (int)Math.Sqrt(numberOfBlocks);
+            var size = updated.First().IndexOf('/');
             var final = new bool[numberPerSide * size, numberPerSide * size];
 
             var blockX = 0;
@@ -104,25 +129,26 @@ namespace Solutions
                 }
             }
 
-            return ArrayToString(final);
+            return final;
         }
 
-        private (List<string>, int size) SplitUpInput(string input)
+        private List<string> SplitUpInput(bool[,] input)
         {
-            var full = StringToArray(input);
-
-            if (full.GetUpperBound(0) + 1 % 2 == 0)
+            if ((input.GetUpperBound(0) + 1) % 2 == 0)
             {
-                return SplitUp(full, 2);
+                return SplitUp(input, 2);
+            }
+            else if ((input.GetUpperBound(0) + 1) % 3 == 0)
+            {
+                return SplitUp(input, 3);
             }
             else
             {
-                return SplitUp(full, 3);
+                throw new Exception("Unknown grid size - " + (input.GetUpperBound(0) + 1));
             }
-
         }
 
-        private (List<string>, int size) SplitUp(bool[,] full, int size)
+        private List<string> SplitUp(bool[,] full, int size)
         {
             var result = new List<string>();
             var numberToGenerate = (full.GetUpperBound(0) + 1) / size;
@@ -143,7 +169,7 @@ namespace Solutions
                 }
             }
 
-            return (result, size);
+            return result;
         }
 
         public Dictionary<string, string> ExpandPatterns(string input)
