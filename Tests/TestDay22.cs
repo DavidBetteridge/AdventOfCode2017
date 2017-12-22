58504be8d76874724d9f5622e971add9f584b535
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-22 08:31:51 +0000

    Day 22 - Part1

---
+++ Tests/TestDay22.cs
@@ -0,0 +1,69 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static Solutions.Day22;
+
+namespace Tests
+{
+    public class TestDay22
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day22 = new Day22();
+            var map = day22.LoadMap(@"..#
+#..
+...");
+            var actual = day22.Part1(map, 70);
+            Assert.Equal(41,actual);
+        }
+
+        [Fact]
+        public void Part1b()
+        {
+            var day22 = new Day22();
+            var map = day22.LoadMap(@"..#
+#..
+...");
+            var actual = day22.Part1(map, 10000);
+            Assert.Equal(5587, actual);
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day22 = new Day22();
+            var map = day22.LoadMap(PUZZLE_INPUT);
+            var actual = day22.Part1(map, 10000);
+            Assert.Equal(5450, actual);
+        }
+
+        private const string PUZZLE_INPUT = @"#.....##.####.#.#########
+.###..#..#..####.##....#.
+..#########...###...####.
+.##.#.##..#.#..#.#....###
+...##....###..#.#..#.###.
+###..#...######.####.#.#.
+#..###..###..###.###.##..
+.#.#.###.#.#...####..#...
+##........##.####..##...#
+.#.##..#.#....##.##.##..#
+###......#..##.####.###.#
+....#..###..#######.#...#
+#####.....#.##.#..#..####
+.#.###.#.###..##.#..####.
+..#..##.###...#######....
+.#.##.#.#.#.#...###.#.#..
+##.###.#.#.###.#......#..
+###..##.#...#....#..####.
+.#.#.....#..#....##..#..#
+#####.#.##..#...##..#....
+##..#.#.#.####.#.##...##.
+..#..#.#.####...#........
+###.###.##.#..#.##.....#.
+.##..##.##...#..#..#.#..#
+#...####.#.##...#..#.#.##";
+    }
+}
