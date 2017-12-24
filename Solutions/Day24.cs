f329eb882f235b7b1719a8d43274ae3673937cfc
David Betteridge <DavidaBetteridge@gmail.com>
2017-12-24 16:08:43 +0000

    Day 24 - Part1

---
+++ Solutions/Day24.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day24
+    {
+        class Block
+        {
+            public int LHS { get; set; }
+            public int RHS { get; set; }
+            public bool Seen { get; set; }
+        }
+
+        public int SolvePart1(string input)
+        {
+            var blocks = input.Split(new[] { System.Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+                               .Select(l => new Block() { LHS = int.Parse(l.Split('/')[0]), RHS = int.Parse(l.Split('/')[1]) })
+                               .ToList();
+
+            return AddNode(blocks, 0);
+        }
+
+        private int AddNode(List<Block> blocks, int connectWith)
+        {
+            var result = 0;
+
+            foreach (var block in blocks.Where(b => (b.LHS == connectWith || b.RHS == connectWith) && !b.Seen))
+            {
+                block.Seen = true;
+                var localResult = block.LHS + block.RHS + AddNode(blocks, (block.LHS == connectWith ? block.RHS : block.LHS));
+                result = Math.Max(result, localResult);
+                block.Seen = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tests/TestDay24.cs
@@ -0,0 +1,92 @@
+﻿using Solutions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class TestDay24
+    {
+        [Fact]
+        public void Part1()
+        {
+            var day24 = new Day24();
+            var actual = day24.SolvePart1(@"0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10");
+            Assert.Equal(31, actual);
+        }
+
+        [Fact]
+        public void Part1_Answer()
+        {
+            var day24 = new Day24();
+            var actual = day24.SolvePart1(PUZZLE_INPUT);
+            Assert.Equal(1868, actual);
+        }
+
+        private const string PUZZLE_INPUT = @"25/13
+4/43
+42/42
+39/40
+17/18
+30/7
+12/12
+32/28
+9/28
+1/1
+16/7
+47/43
+34/16
+39/36
+6/4
+3/2
+10/49
+46/50
+18/25
+2/23
+3/21
+5/24
+46/26
+50/19
+26/41
+1/50
+47/41
+39/50
+12/14
+11/19
+28/2
+38/47
+5/5
+38/34
+39/39
+17/34
+42/16
+32/23
+13/21
+28/6
+6/20
+1/30
+44/21
+11/28
+14/17
+33/33
+17/43
+31/13
+11/21
+31/39
+0/9
+13/50
+10/14
+16/10
+3/24
+7/0
+50/50";
+    }
+}
